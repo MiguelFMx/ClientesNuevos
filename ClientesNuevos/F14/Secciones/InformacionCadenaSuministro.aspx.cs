dd0f18f298f8884d7bbff26d78c1715641f80c5a
MiguelF <freyde.miguel@gmail.com>
2022-09-14 13:42:12 -0700

    prueba

--- ClientesNuevos/F14/Secciones/InformacionCadenaSuministro.aspx.cs
+++ ClientesNuevos/F14/Secciones/InformacionCadenaSuministro.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Optimization;
+
 
 namespace ClientesNuevos.F14.Seccioness
 {
--- ClientesNuevos/login.aspx.cs
+++ ClientesNuevos/login.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
+using System.Web.Optimization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
