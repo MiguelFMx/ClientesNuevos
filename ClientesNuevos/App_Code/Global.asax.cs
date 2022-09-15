c3843bbbe184efef0e92726afadfe379fffe89fa
MiguelF <freyde.miguel@gmail.com>
2022-09-15 12:10:55 -0700

    cambio global

--- ClientesNuevos/App_Code/Global.asax.cs
+++ ClientesNuevos/App_Code/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -12,7 +14,8 @@ namespace ClientesNuevos.App_Code
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Session_Start(object sender, EventArgs e)
