b611ca88154275acfc15225fc112d9bd1e3c0c2b
MiguelF <freyde.miguel@gmail.com>
2022-09-28 14:00:09 -0700

    cambios

--- ClientesNuevos/Global.asax.cs
+++ ClientesNuevos/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
-namespace ClientesNuevos.App_Code
+namespace ClientesNuevos
 {
     public class Global : System.Web.HttpApplication
     {
